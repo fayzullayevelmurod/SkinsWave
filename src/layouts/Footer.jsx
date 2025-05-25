48b0fe73f195a0cc2a1321b117ec4bfa6ac0e5b4
SardorQobilov <sardorqobilov555@gmail.com>
2025-05-26 00:42:35 +0500

    first commit

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className='pt-[70px] pb-[100px] border-y border-[#33353f]'>
+      <div className='container'>
+          <h2 className='text-white text-xl font-normal'>SkinsWave</h2>
+          <div>
+            
+          </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from '../layouts/Footer';
+import Header from '../layouts/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
